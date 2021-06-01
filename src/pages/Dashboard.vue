9ecb78844cf90e2548af3ee47b4cf9085b0b058c
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-06-01 19:29:59 -0300

    mais exemplos de rota

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>teste</h2>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard'
+}
+</script>
+
+<style>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import Login from '@/pages/Login.vue'
 import Home from '@/pages/Homepage.vue'
+import Dashboard from '@/pages/Dashboard.vue'
 
 const routes = [
     {
         path: '/admin',
         component: Login,
         name: 'Login',
+        meta: {title: 'WK3 Admin'}
+    },
+    {
+        path: '/dashboard',
+        component: Dashboard,
+        name: 'Dashboard',
         meta: {title: 'WK3 Admin'}
     },
     {
