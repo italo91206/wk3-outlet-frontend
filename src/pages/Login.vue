cc8857ba711493a81edb1437c7df1dd86bfa13f7
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-06-11 05:25:58 -0300

    estilização login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,32 +1,75 @@
 <template>
-  <form>
-    <div>
-        <label for="">Usuário</label>
-        <input type="text">
-    </div>
+    <main id="admin-login">
+        <div class="wrapper">
+            <form>
+                <h3 class="text-center">Wk3 Outlet</h3>
 
-    <div>
-        <label for="">Senha</label>
-        <input type="password">
-    </div>
+                <div class="form-group">
+                    <label for="">Usuário</label>
+                    <input type="text" v-model="user">
+                </div>
 
-    <div>
-        <input type="button" value="login">
-    </div>
+                <div class="form-group">
+                    <label for="">Senha</label>
+                    <input type="password" v-model="password">
+                </div>
 
-    <div>
-        Desenvolvimento por <a href="https://italoferreira.dev" target="_blank">Italo Ferreira</a>
-    </div>
-
-  </form>
+                <div>
+                    <input type="button" value="login" class="login-btn" @click="fazerLogin">
+                </div>
+            </form>
+        </div>
+    </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Login'
+    name: 'Login',
+    data(){
+        return {
+            user: '',
+            password: '',
+            info: ''
+        }
+    },
+    methods:{
+        fazerLogin(){
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => (this.info = response))
+        }
+    }
 }
 </script>
 
 <style>
+#admin-login{
+    background-color: #373330;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
 
+#admin-login form{
+    max-width: 300px;
+    max-height: 400px;
+    margin: 200px auto;
+}
+
+#admin-login .form-group label{
+    display: block;
+}
+
+.login-btn{
+    width: 100%;
+}
+
+#admin-login form{
+    background-color: #fff;
+    border: 1px solid #e3e3e3;
+    box-shadow: 0 5px 30px 0 #000;
+    padding: 50px;
+}
 </style>
