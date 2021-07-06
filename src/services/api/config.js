30c32200ae09417460e3dc5f6a92c54285a28e4c
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-07-06 07:45:05 -0300

    added API resources

---
+++ src/services/api/config.js
@@ -0,0 +1,11 @@
+const ENVIROMENT = process.env.NODE_ENV;
+
+export const config = {
+    setBaseUrl: () => {
+        let baseURL = `http://localhost:5000/`
+
+        //if(ENVIROMENT === 'production') baseURL = `https://lautech-backend.herokuapp.com/api/v1`
+
+        return baseURL;
+    }
+};
