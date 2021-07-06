6749743d591f119d953e46cf6340717342611314
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-07-06 07:49:58 -0300

    Fixed ENV variable usage

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -4,7 +4,7 @@ export const config = {
     setBaseUrl: () => {
         let baseURL = `http://localhost:5000/`
 
-        //if(ENVIROMENT === 'production') baseURL = `https://lautech-backend.herokuapp.com/api/v1`
+        if(ENVIROMENT === 'production') baseURL = `https://lautech-backend.herokuapp.com/api/v1`
 
         return baseURL;
     }
