827ca563f95cd53c9eb28d0918e6123551fc3d80
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-10 17:53:53 -0300

    added right backend URL

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -4,7 +4,7 @@ export const config = {
     setBaseUrl: () => {
         let baseURL = `http://localhost:5000/`
 
-        if(ENVIROMENT === 'production') baseURL = `https://lautech-backend.herokuapp.com/api/v1`
+        if(ENVIROMENT === 'production') baseURL = `https://wk3-outlet-backend.herokuapp.com/`
 
         return baseURL;
     }
