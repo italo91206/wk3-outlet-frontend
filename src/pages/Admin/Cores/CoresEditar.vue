1bf3c1a592b4b2be5788942ef91f2eb7cff51b30
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-17 18:06:04 -0300

    cores validation added

--- src/pages/Admin/Cores/CoresEditar.vue
+++ src/pages/Admin/Cores/CoresEditar.vue
@@ -4,10 +4,15 @@
       <div class="col-lg-12">
         <div class="card">
           <div class="card-body">
-            <div class="form">
+            <div class="form" @change="mudou">
               <div class="form-group row">
                 <label for="" class="col-sm-2 col-form-label">Nome da cor</label>
-                <input type="text" v-model="corToPost.cor" class="col-sm-8 form-control">
+                <input 
+                  type="text" 
+                  v-model="corToPost.cor" 
+                  class="col-sm-8 form-control"
+                  @change="validarNome">
+                <span class="col-sm-8 text-danger" v-if="erro_nome" style="margin: 0 auto">{{erro_nome}}</span>
               </div>
 
               <div class="form-group row">
@@ -37,7 +42,9 @@ export default {
     name: "CoresEditar",
     data(){
       return {
-        corToPost: {}
+        corToPost: {},
+        isChanged: true,
+        erro_nome: null,
       }
     },
     methods: {
@@ -47,22 +54,46 @@ export default {
       },
       async deletarCor(id){
         const response = await service.deletarCor(id);
-        console.log(response.data);
+        if(response.data.success){
+          this.$toast.success('Cor foi removida com sucesso!');
+          this.$router.push('/admin/cores');
+        }
+        else
+          this.$toast.error(response.data.message);
       },
       async atualizarCor(){
-        const response = await service.atualizarCor(this.corToPost);
-        console.log(response.data)
+        if(this.erro_nome)
+          this.$toast.error('Alguns campos não estão válidos');
+        else{
+          const response = await service.atualizarCor(this.corToPost);
+          if(response.data.success){
+            this.$toast.success('Cor foi alterada com sucesso!');
+            this.$router.push('/admin/cores');
+          }
+          else
+            this.$toast.error(response.data.message);
+        }
+      },
+      mudou(){
+        this.isChanged = false
       },
       deletar(){
         let resposta = confirm('Tem certeza que deseja excluir ? Não há volta.')
         if(resposta)
           this.deletarCor(this.corToPost.cor_id);
+      },
+      validarNome(e){
+        var string = e.target.value;
+        if(/[^A-z\s\d][\\^]?/.test(string))
+          this.erro_nome = "Não é possível inserir caracteres especiais";
+        else
+          this.erro_nome = '';
       }
     },
     mounted() {
       const id = this.$route.params.id;
       this.listarCor(id);
-    }
+    },
 }
 </script>
 
--- src/pages/Admin/Cores/CoresNovo.vue
+++ src/pages/Admin/Cores/CoresNovo.vue
@@ -13,7 +13,9 @@
                   type="text"
                   v-model="corToPost.cor"
                   class="col-sm-8 form-control"
+                  @change="validarNome"
                 />
+                <span class="col-sm-8 text-danger" v-if="erro_nome" style="margin: 0 auto">{{erro_nome}}</span>
               </div>
 
               <div class="form-group row">
@@ -61,12 +63,29 @@ export default {
         hexa: "",
       },
       isChanged: true,
+      erro_nome: null
     };
   },
   methods: {
     async salvar() {
-      const response = await service.salvarCor(this.corToPost);
-      console.log(response.data);
+      if(this.erro_nome)
+        this.$toast.error('Alguns campos não estão válidos');
+      else{
+        const response = await service.salvarCor(this.corToPost);
+        if(response.data.success){
+          this.$toast.success('Cor adicionada com sucesso!');
+          this.$router.push('/admin/cores');
+        }
+        else
+          this.$toast.error(response.data.message);
+      }
+    },
+    validarNome(e){
+      var string = e.target.value;
+      if(/[^A-z\s\d][\\^]?/.test(string))
+        this.erro_nome = "Não é possível inserir caracteres especiais";
+      else
+        this.erro_nome = '';
     },
     mudou() {
       this.isChanged = false;
