77a6d2b1ba7fdea190fa4ec911bc2fe7f0bd6b47
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-16 22:04:10 -0300

    misc fix

--- src/routes.js
+++ src/routes.js
@@ -27,246 +27,246 @@ import CuponsEditar from '@/pages/Admin/Cupons/CuponsEditar.vue'
 import AcertosConfig from '@/pages/Admin/Acertos/AcertosConfig.vue'
 
 const routes = [
-    {
-        path: '/admin',
-        component: Login,
-        name: 'Login',
-        meta: {title: 'WK3 Administrativo'}
-    },
-    {
-        path: '/dashboard',
-        component: Dashboard,
-        name: 'Dashboard',
-        meta: {
-            title: 'WK3 | Dashboard',
-            layout: 'AppLayoutAdmin'    
-        }
-    },
-    {
-        path: '/admin/atributos',
-        component: AtributosConfig,
-        name: 'Todos os atributos',
-        meta: {
-            title: 'WK3 Admin | Atributos',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/tamanhos',
-        component: TamanhosConfig,
-        name: 'Todos os tamanhos',
-        meta: {
-            title: 'WK3 Admin | Tamanhos',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/tamanhos/editar/:id',
-        component: TamanhosEditar,
-        name: 'Editar tamanho',
-        meta: {
-            title: 'WK3 Admin | Editar Tamanho',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/tamanhos/novo',
-        component: TamanhosNovo,
-        name: 'Adicionar novo tamanho',
-        meta: {
-            title: 'WK3 Admin | Novo Tamanho',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/cupons',
-        component: CuponsConfig,
-        name: 'Todos os cupons',
-        meta: {
-            title: 'WK3 Admin | Cupons',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/cupons/novo',
-        component: CuponsNovo,
-        name: 'Adicionar novo cupom',
-        meta: {
-            title: 'WK3 Admin | Novo Cupom',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/cupons/editar/:id',
-        component: CuponsEditar,
-        name: 'Editar cupom',
-        meta: {
-            title: 'WK3 Admin | Editar Cupom',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/acertos',
-        component: AcertosConfig,
-        name: 'Acertos de estoque',
-        meta: {
-            title: 'WK3 Admin | Acertos De Estoque',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/cores',
-        component: CoresConfig,
-        name: 'Todas as cores',
-        meta: {
-            title: 'WK3 Admin | Cores',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/cores/editar/:id',
-        component: CoresEditar,
-        name: 'Editar cor',
-        meta: {
-            title: 'WK3 Admin | Editar Cor',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/cores/novo',
-        component: CoresNovo,
-        name: 'Adicionar nova cor',
-        meta: {
-            title: 'WK3 Admin | Nova Cor',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/usuarios',
-        component: UsuariosConfig,
-        name: 'Todos os usuários',
-        meta: {
-            title: 'WK3 Admin | Usuários',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/usuarios/editar/:id',
-        component: UsuariosEditar,
-        name: 'Editar usuário',
-        meta: {
-            title: 'WK3 Admin | Editar Usuário',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/usuario/novo',
-        component: UsuariosNovo,
-        name: 'Novo usuário',
-        meta: {
-            title: 'WK3 Admin | Novo Usuário',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/marcas',
-        component: MarcasConfig,
-        name: 'Todas as marcas',
-        meta: {
-            title: 'WK3 Admin | Marcas',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/motivos',
-        component: MotivosConfig,
-        name: 'Todos os motivos',
-        meta: {
-            title: 'WK3 Admin | Motivos',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/motivos/editar/:id',
-        component: MotivosEditar,
-        name: 'Editar movo',
-        meta: {
-            title: 'WK3 Admin | Editar Motivo',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/motivos/novo',
-        component: MotivosNovo,
-        name: 'Adicionar novo motivo',
-        meta: {
-            title: 'WK3 Admin | Adicionar Novo Motivo',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/modelos',
-        component: ModelosConfig,
-        name: 'Todos os modelos',
-        meta: {
-            title: 'WK3 Admin | Modelos',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/produtos',
-        component: ProdutosConfig,
-        name: 'Todos os produtos',
-        meta: {
-            title: 'Wk3 Admin | Produtos',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/produtos/novo',
-        component: ProdutosNovo,
-        name: 'Adicionar novo produto',
-        meta: {
-            title: 'Wk3 Admin | Adicionar Novo Produto',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/admin/produtos/editar/:product_url',
-        name: 'Editar produto',
-        component: ProdutosEditar,
-        meta: {
-            title: 'Wk3 Admin | Editar Produto',
-            layout: 'AppLayoutAdmin'
-        }
-    },
-    {
-        path: '/404',
-        component: NaoEncontrado,
-        name: 'Não encontrado',
-        meta: {
-            title: 'WK3 Outlet - Nada foi encontrado',
-            layout: 'AppLayoutDefault'
-        }
-    },
-    {
-        path: '/',
-        component: Home,
-        name: 'Home',
-        meta: {
-            title: 'WK3 Outlet',
-            layout: 'AppLayoutDefault'
-        }
-    },
-    {
-        path: '/:product_url',
-        component: ProductPage,
-        name: 'WK3 Outlet | Página de produto',
-        meta: {
-            title: 'WK3 Homepage',
-            layout: 'AppLayoutDefault'
-        }
+  {
+    path: '/admin',
+    component: Login,
+    name: 'Login',
+    meta: { title: 'WK3 Administrativo' }
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard,
+    name: 'Dashboard',
+    meta: {
+      title: 'WK3 | Dashboard',
+      layout: 'AppLayoutAdmin'
     }
+  },
+  {
+    path: '/admin/atributos',
+    component: AtributosConfig,
+    name: 'Todos os atributos',
+    meta: {
+      title: 'WK3 Admin | Atributos',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/tamanhos',
+    component: TamanhosConfig,
+    name: 'Todos os tamanhos',
+    meta: {
+      title: 'WK3 Admin | Tamanhos',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/tamanhos/editar/:id',
+    component: TamanhosEditar,
+    name: 'Editar tamanho',
+    meta: {
+      title: 'WK3 Admin | Editar Tamanho',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/tamanhos/novo',
+    component: TamanhosNovo,
+    name: 'Adicionar novo tamanho',
+    meta: {
+      title: 'WK3 Admin | Novo Tamanho',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/cupons',
+    component: CuponsConfig,
+    name: 'Todos os cupons',
+    meta: {
+      title: 'WK3 Admin | Cupons',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/cupons/novo',
+    component: CuponsNovo,
+    name: 'Adicionar novo cupom',
+    meta: {
+      title: 'WK3 Admin | Novo Cupom',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/cupons/editar/:id',
+    component: CuponsEditar,
+    name: 'Editar cupom',
+    meta: {
+      title: 'WK3 Admin | Editar Cupom',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/acertos',
+    component: AcertosConfig,
+    name: 'Acertos de estoque',
+    meta: {
+      title: 'WK3 Admin | Acertos De Estoque',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/cores',
+    component: CoresConfig,
+    name: 'Todas as cores',
+    meta: {
+      title: 'WK3 Admin | Cores',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/cores/editar/:id',
+    component: CoresEditar,
+    name: 'Editar cor',
+    meta: {
+      title: 'WK3 Admin | Editar Cor',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/cores/novo',
+    component: CoresNovo,
+    name: 'Adicionar nova cor',
+    meta: {
+      title: 'WK3 Admin | Nova Cor',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/usuarios',
+    component: UsuariosConfig,
+    name: 'Todos os usuários',
+    meta: {
+      title: 'WK3 Admin | Usuários',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/usuarios/editar/:id',
+    component: UsuariosEditar,
+    name: 'Editar usuário',
+    meta: {
+      title: 'WK3 Admin | Editar Usuário',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/usuario/novo',
+    component: UsuariosNovo,
+    name: 'Novo usuário',
+    meta: {
+      title: 'WK3 Admin | Novo Usuário',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/marcas',
+    component: MarcasConfig,
+    name: 'Todas as marcas',
+    meta: {
+      title: 'WK3 Admin | Marcas',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/motivos',
+    component: MotivosConfig,
+    name: 'Todos os motivos',
+    meta: {
+      title: 'WK3 Admin | Motivos',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/motivos/editar/:id',
+    component: MotivosEditar,
+    name: 'Editar movo',
+    meta: {
+      title: 'WK3 Admin | Editar Motivo',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/motivos/novo',
+    component: MotivosNovo,
+    name: 'Adicionar novo motivo',
+    meta: {
+      title: 'WK3 Admin | Adicionar Novo Motivo',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/modelos',
+    component: ModelosConfig,
+    name: 'Todos os modelos',
+    meta: {
+      title: 'WK3 Admin | Modelos',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/produtos',
+    component: ProdutosConfig,
+    name: 'Todos os produtos',
+    meta: {
+      title: 'Wk3 Admin | Produtos',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/produtos/novo',
+    component: ProdutosNovo,
+    name: 'Adicionar novo produto',
+    meta: {
+      title: 'Wk3 Admin | Adicionar Novo Produto',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/admin/produtos/editar/:product_url',
+    name: 'Editar produto',
+    component: ProdutosEditar,
+    meta: {
+      title: 'Wk3 Admin | Editar Produto',
+      layout: 'AppLayoutAdmin'
+    }
+  },
+  {
+    path: '/404',
+    component: NaoEncontrado,
+    name: 'Não encontrado',
+    meta: {
+      title: 'WK3 Outlet - Nada foi encontrado',
+      layout: 'AppLayoutDefault'
+    }
+  },
+  {
+    path: '/',
+    component: Home,
+    name: 'Home',
+    meta: {
+      title: 'WK3 Outlet',
+      layout: 'AppLayoutDefault'
+    }
+  },
+  {
+    path: '/:product_url',
+    component: ProductPage,
+    name: 'WK3 Outlet | Página de produto',
+    meta: {
+      title: 'WK3 Homepage',
+      layout: 'AppLayoutDefault'
+    }
+  }
 ]
 
 export default routes;
