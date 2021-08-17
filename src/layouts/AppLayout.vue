75988d322f23df2b0fe973ca182a63e9c7d1305c
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-17 18:05:52 -0300

    async scripts added

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,20 +1,38 @@
 <template>
   <component :is="layout">
-    <slot/>
+    <slot />
   </component>
 </template>
 
 <script>
-const defaultLayout = 'AppLayoutDefault'
+const defaultLayout = "AppLayoutDefault";
 
 export default {
-    name: "AppLayout",
-    computed: {
-        layout(){
-            const name = this.$route.meta.layout || 'None'
-            const layout = name || defaultLayout
-            return () => import(`@/layouts/${layout}.vue`)
-        }
-    }
-}
+  name: "AppLayout",
+  computed: {
+    layout() {
+      const name = this.$route.meta.layout || "None";
+      const layout = name || defaultLayout;
+      return () => import(`@/layouts/${layout}.vue`);
+    },
+  },
+  mounted() {
+    let jqueryScript = document.createElement('script');
+    jqueryScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js');
+
+    let bootstrap = document.createElement('script');
+    bootstrap.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.bundle.min.js')
+    
+    let adminlte = document.createElement('script');
+    adminlte.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.0/js/adminlte.min.js');
+
+    let dashboard3 = document.createElement('script');
+    dashboard3.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.0/js/pages/dashboard3.min.js');
+
+    document.head.appendChild(jqueryScript);
+    document.head.appendChild(bootstrap);
+    document.head.appendChild(adminlte);
+    document.head.appendChild(dashboard3);
+  }
+};
 </script>
