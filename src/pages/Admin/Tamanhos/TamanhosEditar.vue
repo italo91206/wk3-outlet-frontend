8dcc6f0d6adc60dbbefcf7d2d664bed7a64ea002
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-08-17 18:06:36 -0300

    tamanhos crud fix

--- src/pages/Admin/Tamanhos/TamanhosEditar.vue
+++ src/pages/Admin/Tamanhos/TamanhosEditar.vue
@@ -7,7 +7,11 @@
             <div class="form" @change="mudou">
               <div class="form-group row">
                 <label for="" class="col-sm-2 col-form-label">Nome do tamanho</label>
-                <input type="text" v-model="tamanhoToPost.tamanho" class="col-sm-8 form-control">
+                <input type="text" 
+                  v-model="tamanhoToPost.tamanho" 
+                  @change="validarNome"
+                  class="col-sm-8 form-control">
+                <span class="col-sm-8 text-danger" v-if="erro_nome" style="margin: 0 auto">{{erro_nome}}</span>
               </div>
             </div>
           </div>
@@ -26,47 +30,70 @@
 </template>
 
 <script>
-import service from '@/services/tamanhos/tamanhos-service.js'
+import service from "@/services/tamanhos/tamanhos-service.js";
 
 export default {
-  name: 'TamanhosEditar',
-  data(){
+  name: "TamanhosEditar",
+  data() {
     return {
       tamanhoToPost: {},
-      isChange: true
-    }
+      isChange: true,
+      erro_nome: null,
+    };
   },
   methods: {
-    async listarTamanho(id){
+    async listarTamanho(id) {
       const response = await service.listarTamanho(id);
       this.tamanhoToPost = response.data;
     },
-    async salvarMotivo(){
-      const response = await service.atualizarTamanho(this.tamanhoToPost);
-      console.log(response.data);
+    async salvarMotivo() {
+      if(this.erro_nome)
+        this.$toast.error('Alguns campos não estão válidos');
+      else{
+        const response = await service.atualizarTamanho(this.tamanhoToPost);
+        if(response.data.success){
+          this.$toast.success('Tamanho foi atualizado com sucesso');
+          this.$router.push('/admin/tamanhos');
+        }
+        else
+          this.$toast.error(response.data.message);
+      }
     },
-    async deletarTamanho(id){
+    async deletarTamanho(id) {
       const response = await service.deletarTamanho(id);
-      console.log(response.data);
+      if(response.data.success){
+        this.$toast.success('Tamanho removido com sucesso');
+        this.$router.push('/admin/tamanhos');
+      }
+      else
+        this.$toast.error(response.data.message);
     },
-    mudou(){
+    mudou() {
       this.isChange = false;
     },
-    deletar(){
-      const resposta = confirm('Deseja mesmo deletar este tamanho? Esta ação é irreversível');
-      if(resposta){
+    deletar() {
+      const resposta = confirm(
+        "Deseja mesmo deletar este tamanho? Esta ação é irreversível"
+      );
+      if (resposta) {
         const id = this.$route.params.id;
         this.deletarTamanho(id);
       }
+    },
+    validarNome(e){
+      var string = e.target.value;
+      if(/[^A-z\s\d][\\^]?/.test(string))
+        this.erro_nome = "Não é possível inserir caracteres especiais";
+      else
+        this.erro_nome = '';
     }
   },
-  mounted(){
+  mounted() {
     const id = this.$route.params.id;
     this.listarTamanho(id);
-  }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/pages/Admin/Tamanhos/TamanhosNovo.vue
+++ src/pages/Admin/Tamanhos/TamanhosNovo.vue
@@ -7,7 +7,8 @@
             <div class="form">
               <div class="form-group row">
                 <label for="" class="col-sm-2 col-form-label">Nome do tamanho</label>
-                <input type="text" v-model="tamanhoToPost.tamanho" class="col-sm-8 form-control">
+                <input type="text" @change="validarNome" v-model="tamanhoToPost.tamanho" class="col-sm-8 form-control">
+                <span class="col-sm-8 text-danger" v-if="erro_nome" style="margin: 0 auto">{{erro_nome}}</span>
               </div>
             </div>
           </div>
@@ -33,13 +34,30 @@ export default {
     return {
       tamanhoToPost: {
         tamanho: ''
-      }
+      },
+      erro_nome: null,
     }
   },
   methods:{
     async novo(){
-      const response = await service.novoTamanho(this.tamanhoToPost);
-      console.log(response.data);
+      if(this.erro_nome)
+        this.$toast.error('Alguns campos não estão válidos');
+      else{
+        const response = await service.novoTamanho(this.tamanhoToPost);
+        if(response.data.success){
+          this.$toast.success('Tamanho foi cadastrado com sucesso');
+          this.$router.push('/admin/tamanhos');
+        }
+        else
+          this.$toast.error(response.data.message);
+      }
+    },
+    validarNome(e){
+      var string = e.target.value;
+      if(/[^A-z\s\d][\\^]?/.test(string))
+        this.erro_nome = "Não é possível inserir caracteres especiais";
+      else
+        this.erro_nome = '';
     }
   }
 }
