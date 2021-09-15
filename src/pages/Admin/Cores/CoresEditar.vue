7c4310798be2fbb4192dc6990fd21a661b57b938
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-09-15 20:32:13 -0300

    unecessary code removed

--- src/pages/Admin/Cores/CoresEditar.vue
+++ src/pages/Admin/Cores/CoresEditar.vue
@@ -33,41 +33,6 @@
       </v-row>
     </v-container>
   </v-main>
-
-  <!-- <main id="cores">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card">
-          <div class="card-body">
-            <div class="form" @change="mudou">
-              <div class="form-group row">
-                <label for="" class="col-sm-2 col-form-label">Nome da cor</label>
-                <input 
-                  type="text" 
-                  v-model="corToPost.cor" 
-                  class="col-sm-8 form-control"
-                  @change="validarNome">
-                <span class="col-sm-8 text-danger" v-if="erro_nome" style="margin: 0 auto">{{erro_nome}}</span>
-              </div>
-
-              <div class="form-group row">
-                <label for="" class="col-sm-2 col-form-label">Cor</label>
-                <input type="color" v-model="corToPost.hexa" class="col-sm-8 form-control">
-              </div>
-            </div>
-            
-            <div class="row">
-              <div class="col-lg-9">
-                <button class="btn btn-primary float-right" :disabled="isChanged" @click="atualizarCor">Salvar</button>
-                <button @click="deletar" class="btn btn-danger float-right">Deletar</button>
-                <router-link to="/admin/cores" class="btn btn-default float-right">Voltar</router-link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main> -->
 </template>
 
 <script>
--- src/pages/Admin/Cores/CoresNovo.vue
+++ src/pages/Admin/Cores/CoresNovo.vue
@@ -32,57 +32,6 @@
       </v-row>
     </v-container>
   </v-main>
-  
-  <!-- <main id="cores">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card">
-          <div class="card-body">
-            <div class="form" @change="mudou">
-              <div class="form-group row">
-                <label for="" class="col-sm-2 col-form-label"
-                  >Nome da cor</label
-                >
-                <input
-                  type="text"
-                  v-model="corToPost.cor"
-                  class="col-sm-8 form-control"
-                  @change="validarNome"
-                />
-                <span class="col-sm-8 text-danger" v-if="erro_nome" style="margin: 0 auto">{{erro_nome}}</span>
-              </div>
-
-              <div class="form-group row">
-                <label for="" class="col-sm-2 col-form-label">Cor</label>
-                <input
-                  type="color"
-                  v-model="corToPost.hexa"
-                  class="col-sm-8 form-control"
-                />
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col-lg-9">
-                <button
-                  class="btn btn-success float-right"
-                  :disabled="isChanged"
-                  @click="salvar"
-                >
-                  Salvar
-                </button>
-                <router-link
-                  to="/admin/cores"
-                  class="btn btn-default float-right"
-                  >Voltar</router-link
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main> -->
 </template>
 
 <script>
