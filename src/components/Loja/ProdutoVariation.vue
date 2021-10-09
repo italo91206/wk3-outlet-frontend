9ea3d11fda839a10060961ebe0c84dff0ac47730
Italo Ferreira <italoferreiradjk14@gmail.com>
2021-10-08 22:04:51 -0300

    initial scramble of variations available

--- src/components/Loja/ProdutoVariation.vue
+++ src/components/Loja/ProdutoVariation.vue
@@ -1,11 +1,22 @@
 <template>
-  <section>
-    <div v-for="variacao in data" :key="variacao.variacao_id" class="variacao variacao-cor">
-      Cor: {{variacao.cor_id}}
+  <section class="container">
+    <h2>Cores :</h2>
+    <div class="flex">
+      <button 
+        @click="selecionaCor(cor.cor)"
+        v-for="cor in cores" 
+        :key="cor.cor_id" class="variacao variacao-cor" :style="`background-color: ${cor.hexa}`">
+        {{cor.cor}}
+      </button>
     </div>
 
-    <div v-for="variacao in data" :key="variacao.variacao_id" class="variacao variacao-tamanho">
-      Tamanho: {{variacao.tamanho_id}}
+    <h2>Tamanhos :</h2>
+    <div class="flex">
+      <button 
+        @click="selecionaTamanho(tamanho.tamanho)"
+        v-for="tamanho in tamanhos" :key="tamanho.tamanho_id" class="variacao variacao-tamanho">
+        {{tamanho.tamanho}}
+      </button>
     </div>
   </section>
 </template>
@@ -16,12 +27,44 @@ export default {
   props: ['data'],
   data(){
     return {
-      msg: null,
+      cores: [],
+      tamanhos: [],
     }
+  },
+  methods: {
+    processaVariacoes(){
+      let data = this.data;
+      // primeira iteração para as cores
+      data.forEach((variacao) => {
+        if(variacao.cor)
+          this.cores.push(variacao);
+      })
+
+      // segunda iteração para os tamanhos
+      data.forEach((variacao) => {
+        if(variacao.tamanho)
+          this.tamanhos.push(variacao);
+      })
+    },
+    selecionaCor(cor){
+      let resultado = this.data.filter((variacao) => { 
+        return variacao.cor == cor;
+      });
+      this.tamanhos = resultado;
+    },
+    selecionaTamanho(tamanho){
+      const resultado = this.data.filter(variacao => { return variacao.tamanho == tamanho });
+      console.log(resultado);
+    }
+  },
+  mounted() {
+    this.processaVariacoes();
   }
 }
 </script>
 
 <style lang="css">
-
+button.variacao {
+  border: solid 1px grey;
+}
 </style>
