46a1b50079fdd84d447a9c5c5564b78979eee8cc
italo91206 <italoferreiradjk14@gmail.com>
2022-02-21 17:19:21 -0300

    document formatting

--- src/services/imagens/imagem-service.js
+++ src/services/imagens/imagem-service.js
@@ -1,38 +1,38 @@
 import api from '../api/instanceApi';
 
 const ImagemService = {
-	enviarImagens: async(formData) => {
-		try {
-			const response = await api.post('/imagens/upload', formData, {
-				headers: {
-					"Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
-				}
-			})
-			return response;
-		}
-		catch(error){
-			console.log(error)
-		}
-	},
-	recuperarImagens: async(id) => {
-		try {
-			const response = await api.get('/imagens/recuperar', {
-				params: { id: id },
-			});
-			return response;
-		}
-		catch(error){
-			console.log(error);
-		}
-  },
-  removerImagem: async(id) => {
+  enviarImagens: async (formData) => {
     try {
-      const response = await api.delete('/imagens/remover', { 
+      const response = await api.post('/imagens/upload', formData, {
+        headers: {
+          "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+        }
+      })
+      return response;
+    }
+    catch (error) {
+      console.log(error)
+    }
+  },
+  recuperarImagens: async (id) => {
+    try {
+      const response = await api.get('/imagens/recuperar', {
+        params: { id: id },
+      });
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  },
+  removerImagem: async (id) => {
+    try {
+      const response = await api.delete('/imagens/remover', {
         params: { id: id }
       });
       return response;
     }
-    catch(error){
+    catch (error) {
       console.log(error);
     }
   }
