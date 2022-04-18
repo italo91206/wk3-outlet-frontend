20d61ec91d159f4d6f9cad42675533e256090a7d
italo91206 <italoferreiradjk14@gmail.com>
2022-04-18 18:48:02 -0300

    better formatting

--- src/services/api/instanceApi.js
+++ src/services/api/instanceApi.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
-import {config} from './config';
+import { config } from './config';
 
 const bURL = config.setBaseUrl();
 
 const configApiLocal = {
-    baseURL: `${bURL}`,
-    responseType: 'json',
+  baseURL: `${bURL}`,
+  responseType: 'json',
 };
 
 const api = axios.create(configApiLocal);
 
 api.interceptors.request.use(async (config) => {
-    const token = localStorage.getItem('user');
+  const token = localStorage.getItem('user');
 
-    if (token) config.headers.Authorization = token;
-    return config;
+  if (token) config.headers['x-access-token'] = token;
+  return config;
 });
 
 export default api;
